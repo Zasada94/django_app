6e62617fcea89e3a27717421d5b15f5c3c7c8321
Zasada94 <pawelzasada2@wp.pl>
2024-05-01 12:54:05 +0200

    added registration view and coneccted auth routes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,9 +5,9 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fieilds = ["id", "username", "password"]
+        fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objectrs.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
